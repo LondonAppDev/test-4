2a412fa98ea3a2e97f2f3e2195f077ad9cef5d2a
Mark Winterbottom <mark@londonappdeveloper.com>
2021-01-30 17:55:14 +0000

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = '/vol/web/media'
-STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = 'vol/web/media'
+STATIC_ROOT = '/vol/web/static/'
 
 AUTH_USER_MODEL = 'core.User'
